da34aeb1a354390ec7cadb9254ad1f42bccd18d0
Meysam Amirsardari <62254770+MeysamAmirsardari@users.noreply.github.com>
2022-08-24 19:15:35 +0430

    to be

--- producer.go
+++ producer.go
@@ -7,10 +7,12 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 )
 
 func main() {
-	p := Ininitialize()
+	p := Initialize()
 	pathList := creatPathList2("C:/Users/EMINENT/Desktop/Tests/*.csv")
 
 	DoItForMe(pathList, p)
@@ -20,7 +22,7 @@ func main() {
 	p.Close()
 }
 
-func Ininitialize() *kafka.Producer {
+func Initialize() *kafka.Producer {
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s <config-file-path>\n",
 			os.Args[0])
@@ -39,22 +41,26 @@ func Ininitialize() *kafka.Producer {
 	return p
 }
 
-func Send(line []string, p *kafka.Producer) {
+func Send(line []string, p *kafka.Producer, wg *sync.WaitGroup) {
 	topic := "landmarks"
+	wg.Done()
 
-	go func() {
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Failed to deliver message: %v\n", ev.TopicPartition)
-				} else {
-					fmt.Printf("Produced event to topic %s: key = %-10s value = %s\n",
-						*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
-				}
-			}
-		}
-	}()
+	//wg.Add(1)
+	//go func() {
+	//	defer wg.Done()
+	//	for e := range p.Events() {
+	//		switch ev := e.(type) {
+	//		case *kafka.Message:
+	//			if ev.TopicPartition.Error != nil {
+	//				fmt.Printf("Failed to deliver message: %v\n", ev.TopicPartition)
+	//			} else {
+	//				//fmt.Printf("Produced event to topic %s: key = %-10s value = %s\n",
+	//					*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
+	//				fmt.Printf("Produced: value = %s, timestamp = %v, topic = %s \n", string(ev.Value), ev.Timestamp, ev.TopicPartition)
+	//			}
+	//		}
+	//	}
+	//}()
 
 	key := line[0]
 	val := line[1]
@@ -63,6 +69,7 @@ func Send(line []string, p *kafka.Producer) {
 		Key:            []byte(key),
 		Value:          []byte(val),
 	}, nil)
+	return
 }
 
 func creatPathList2(pattern string) []string {
@@ -75,6 +82,10 @@ func creatPathList2(pattern string) []string {
 
 func DoItForMe(pathList []string, p *kafka.Producer) {
 	//var lines []Record //
+	t := time.Now()
+	count := 0
+	var wg sync.WaitGroup
+
 	for _, file := range pathList {
 		f, err := os.Open(file)
 		if err != nil {
@@ -88,10 +99,18 @@ func DoItForMe(pathList []string, p *kafka.Producer) {
 		}
 
 		for _, line := range data {
-			go Send(line, p)
+			wg.Add(1)
+			go Send(line, p, &wg)
 		}
+
+		count += len(data)
 
 		f.Close()
 	}
+
+	wg.Wait()
+
+	deltaT := time.Since(t).Seconds()
+	fmt.Printf("handler took %f, count: %d, tps: %f \n", deltaT, count, float64(count)/deltaT)
 	return
 }
